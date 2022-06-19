016c2db1b9f29109822c0033f903aa69e61e76e9
mhtaldmr <mht.alidemir@gmail.com>
2022-06-19 20:59:18 +0300

    Mocking a dependency service.

---
+++ TestSamurai.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using TestSamurai.Mocking;
+
+namespace TestSamurai.UnitTests.Mocking
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_ShouldStoreTheOrder()
+        {
+            var mockStorage = new Mock<IStorage>();
+            var service = new OrderService(mockStorage.Object);
+
+            var order = new Order();
+            service.PlaceOrder(order);
+
+            mockStorage.Verify(m => m.Store(order));
+        }
+    }
+}
---
+++ TestSamurai/Mocking/OrderService.cs
@@ -0,0 +1,30 @@
+﻿namespace TestSamurai.Mocking
+{
+    public class OrderService
+    {
+        private readonly IStorage _storage;
+
+        public OrderService(IStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public int PlaceOrder(Order order)
+        {
+            int orderId = _storage.Store(order);
+
+            // Some other work
+
+            return orderId;
+        } 
+    }
+
+    public class Order
+    {
+    }
+
+    public interface IStorage
+    {
+        int Store(object obj);
+    }
+}
